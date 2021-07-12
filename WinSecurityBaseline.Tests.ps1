cfcccd512b290810424c125bab7352c5a533ed28
Drew Kennedy <drewkenn@gmail.com>
2021-07-12 13:59:02 +1000

    Update WinSecurityBaseline.Tests.ps1

--- WinSecurityBaseline.Tests.ps1
+++ WinSecurityBaseline.Tests.ps1
@@ -4,13 +4,10 @@ param(
 )
 
 BeforeAll {
-    if (!(choco --version))
-    {
+    if (!(choco --version)) {
         Write-Host "Choco not installed - Installing..."
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     }
-    Write-Host "Hey Hey hey"
-
 
     $WorkingFiles = Get-ChildItem -Recurse
     Write-Host $WorkingFiles
@@ -18,14 +15,16 @@ BeforeAll {
     $Nupkg = $WorkingFiles | Where-Object {($_.Extension -eq '.nupkg') -and ($_.Name -match "$PackageName")}
     Write-Host $Nupkg
 
+    <#Debug Tesging
     $NupkgOutput = choco install $Nupkg.FullName -y
     Write-Host $NupkgOutput
     $ChocoLogs = Get-Content -Path "$($env:programdata)\chocolatey\logs\chocolatey.log"
     Write-Host $ChocoLogs
+    #>
 }
 
 Describe 'Chocolatey Packages Install' {
-    It "Install: $PackageName" {
+    It "Install: $PackageName" -TestCases @{PackageName = $PackageName; Nupkg = $Nupkg}  {
         $PkgInstall = $null
         $PkgInstall = choco install $Nupkg.FullName -y
         $PkgInstall | Where-Object {$_ -match "The install of $PackageName was successful"} | Should -Not -Be $null
@@ -33,7 +32,7 @@ Describe 'Chocolatey Packages Install' {
 }
 
 Describe 'Chocolatey Package is listed' {
-    It "Listed" {
+    It "Listed" -TestCases @{PackageName = $PackageName} {
         $PkgList = $null
         $PkgList = choco list --local-only
         $PkgList | Where-Object {$_ -match $PackageName} | Should -Not -Be $null
@@ -41,7 +40,7 @@ Describe 'Chocolatey Package is listed' {
 }
 
 Describe 'Chocolatey Package Contents exist' {
-    It "$PackageName" {
+    It "$PackageName" -TestCases @{PackageName = $PackageName} {
         $PkgContents = $null
         $PkgContents = Get-ChildItem -Path "${env:ProgramFiles(x86)}\$PackageName" -Recurse
         $PkgContents | Should -Not -Be $null
@@ -49,7 +48,7 @@ Describe 'Chocolatey Package Contents exist' {
 }
 
 Describe 'Chocolatey Package Uninstall' {
-    It "Uninstall: $PackageName" {
+    It "Uninstall: $PackageName" -TestCases @{PackageName = $PackageName} {
         $PkgUninstall = $null
         $PkgUninstall = choco uninstall $PackageName -y
         $PkgUninstall | Where-Object {$_ -match "$PackageName has been successfully uninstalled"} | Should -Not -Be $null
