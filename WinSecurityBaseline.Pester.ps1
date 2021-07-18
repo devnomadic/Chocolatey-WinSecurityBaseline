05dd2a0e5fd47a3595ebe091c9912be9bca3e268
devnomaidc <drewkenn@gmail.com>
2021-07-18 14:45:07 +1000

    updates

--- WinSecurityBaseline.Pester.ps1
+++ WinSecurityBaseline.Pester.ps1
@@ -3,6 +3,8 @@ Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module -Name Pester -MinimumVersion 5.2.2 -Confirm:$false -Force
 
 $outputFile = "$PSScriptRoot\Test-Pester.XML"
+$outputFormat = 'JUnitXml'
+
 $pesterConfigurationContainer = New-PesterContainer -Path "WinSecurityBaseline.Tests.ps1" -Data @{PackageName = 'WinSecurityBaseline'}
 
 $pesterConfiguration = [PesterConfiguration]::Default
@@ -10,6 +12,7 @@ $pesterConfiguration.Run.Container = $pesterConfigurationContainer
 $pesterConfiguration.Run.PassThru = $true
 $pesterConfiguration.TestResult.Enabled = $true
 $pesterConfiguration.TestResult.OutputPath = $outputFile
+$pesterConfiguration.TestResult.OutputFormat = $outputFormat
 $pesterConfiguration.Output.Verbosity = "Detailed"
 #$pesterConfiguration.Debug.WriteDebugMessages = $true
 
