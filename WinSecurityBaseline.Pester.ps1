ef815bd4067305e2c4caad32e9af2054dd322900
devnomaidc <drewkenn@gmail.com>
2021-07-18 21:44:56 +1000

    updates

--- WinSecurityBaseline.Pester.ps1
+++ WinSecurityBaseline.Pester.ps1
@@ -1,9 +1,12 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$OutputFormat = 'JUnitXml'
+)
 
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module -Name Pester -MinimumVersion 5.2.2 -Confirm:$false -Force
 
-$outputFile = "$PSScriptRoot\Test-Pester.XML"
-$outputFormat = 'JUnitXml'
+$outputFile = "$PSScriptRoot\Pester-Tests.xml"
 
 $pesterConfigurationContainer = New-PesterContainer -Path "WinSecurityBaseline.Tests.ps1" -Data @{PackageName = 'WinSecurityBaseline'}
 
@@ -20,3 +23,7 @@ $pesterResult = Invoke-Pester -Configuration $pesterConfiguration
 
 Write-Host "Failed Test Output:"
 $pesterResult.failed.StandardOutput
+
+if ($pesterResult.FailedCount -gt 0) { 
+    throw "$($pesterResult.FailedCount) Tests Failed."
+}
